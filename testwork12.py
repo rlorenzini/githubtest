1f9e08ff0b230a34e97122443c59c905e8e94ab6
Richard Lorenzini <richardlorenzini@richards-mbp.attlocal.net>
2019-02-17 16:39:39 -0600

    printfirstlast

---
+++ testwork12.py
@@ -0,0 +1,12 @@
+#make a list
+#print first and last index
+
+list = [1,2,3,4,5,"cat"]
+just_for_kicks = ["a","b","c"]
+
+def array_stuff(a):
+    num = len(a) -1
+    print(a[0])
+    print(a[num])
+array_stuff(list)
+array_stuff(just_for_kicks)
